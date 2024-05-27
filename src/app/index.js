b0797a51fd70ff5c2b32aaee5d7a828fd61c35fa
DanfordGidraph <danfordmaina@gmail.com>
2024-05-27 19:03:44 +0300

    * feat(app): add support for generating story videos

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const { generatePlayAudio } = require('../utils/AudioGenerator/index.js')
 const { generatePlayVideo } = require('../utils/VideoGenerator/index.js')
-const { generatePlay } = require('../utils/StoryGenerator/index.js')
+const { generatePlay, generateStory } = require('../utils/StoryGenerator/index.js')
 const { writeFileSync } = require('fs')
 const { generateProjectImages } = require('../utils/ImageProcessor/index.js')
 
@@ -14,28 +14,64 @@ const playsDir = path.resolve(__dirname, '../data/json/plays/')
 //     return "Echoes_of_the_Enchanted_Forest"
 // }
 
+
 // tempAsync()
-generatePlay()
-    .then((playData = {}) => {
-        const projectTitle = playData.play.title.trim().replace(/[^\w]/gi, '_')
-        console.log("\nMain:: Play Title => ", projectTitle)
-        writeFileSync(path.join(playsDir, `${projectTitle}.json`), JSON.stringify(playData, null, 4))
-        return projectTitle
-    })
-    .then(async projectTitle => {
-        await generateProjectImages(projectTitle)
-        return projectTitle
-    })
-    .then(async projectTitle => {
-        await generatePlayAudio(projectTitle)
-        console.log("Audio generated successfully")
-        return projectTitle
-    })
-    .then(async projectTitle => {
-        await generatePlayVideo(projectTitle)
-        console.log("Video generated successfully")
-    })
-    .catch(e => {
-        console.log("Error generating video", e)
-    })
+
+const createPlayVideo = async (prompt = "") => {
+    generatePlay(null, null, prompt)
+        .then((playData = {}) => {
+            const projectTitle = playData.play.title.trim().replace(/[^\w]/gi, '_')
+            console.log("\nMain:: Play Title => ", projectTitle)
+            writeFileSync(path.join(playsDir, `${projectTitle}.json`), JSON.stringify(playData, null, 4))
+            return projectTitle
+        })
+        .then(async projectTitle => {
+            await generateProjectImages(projectTitle)
+            return projectTitle
+        })
+        .then(async projectTitle => {
+            await generatePlayAudio(projectTitle)
+            console.log("Audio generated successfully")
+            return projectTitle
+        })
+        .then(async projectTitle => {
+            await generatePlayVideo(projectTitle)
+            console.log("Video generated successfully")
+        })
+        .catch(e => {
+            console.log("Error generating video", e)
+        })
+}
+
+const createStoryVideo = async (prompt = "") => {
+    generateStory(null, null, prompt)
+        .then((storyData = {}) => {
+            const projectTitle = storyData.story.title.trim().replace(/[^\w]/gi, '_')
+            console.log("\nMain:: Story Title => ", projectTitle)
+            writeFileSync(path.join(playsDir, `${projectTitle}.json`), JSON.stringify(storyData, null, 4))
+            return projectTitle
+        })
+        .then(async projectTitle => {
+            await generateProjectImages(projectTitle)
+            return projectTitle
+        })
+        .then(async projectTitle => {
+            await generatePlayAudio(projectTitle)
+            console.log("Audio generated successfully")
+            return projectTitle
+        })
+        .then(async projectTitle => {
+            await generatePlayVideo(projectTitle)
+            console.log("Video generated successfully")
+        })
+        .catch(e => {
+            console.log("Error generating video", e)
+        })
+}
+
+const playPrompt = ""
+const storyPrompt = ""
+
+createPlayVideo(playPrompt)
+// createStoryVideo(storyPrompt)
 
